f9b58cb01a18e6d9d203f9aabc287cbecccb9dae
omere <omerelinav@gmail.com>
2020-11-20 12:05:28 +0200

    rename

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(
     name='bunnies',
     version='0.1dev',
-    packages=['castlyst', ],
+    packages=['bunnies', ],
     long_description=open('README.md').read(),
 )
